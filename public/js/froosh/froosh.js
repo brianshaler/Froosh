9fecc934008fbd8da49c7408939086675ae89578
Brian Shaler <brian@shaler.name>
2011-08-14 15:37:53 -0400

    mobile

--- public/js/froosh/froosh.js
+++ public/js/froosh/froosh.js
@@ -36,7 +36,23 @@ function gotNearby (data) {
     $("#results").html("<ul id=\"resultList\"></ul>");
     data.forEach(function (restaurant) {
         var li = $("<li>");
-        li.html(restaurant.name);
+        var divMessage = $('<div class="resultSpecial">');
+        divMessage.html(restaurant.latest_deal);
+        var divDetails = $('<div class="resultDetails">');
+        var divRestaurant = $('<div class="resultRestaurant">');
+        divRestaurant.html(restaurant.name);
+        divDetails.append(divRestaurant);
+        if (lat && lng && location.restaurant.lat && location.restaurant.lmg) {
+            var divDistance = $('<div class="resultDistance">');
+            var dist = geoDistance(restaurant.loc, {lat: lat, lng: lng});
+            divDistance.html(restaurant.distance);
+            divDetails.append(divDistance);
+        }
+        
+        if (restaurant.latest_deal.length > 0) {
+            li.append(divMessage);
+        }
+        li.append(divDetails);
         $("#resultList").append(li);
     });
     showView("results");
@@ -45,6 +61,26 @@ function gotNearby (data) {
 function deniedLocation () {
     // :-3(
     showView("requiresLocation");
+}
+
+function geoDistance (loc1, loc2) {
+    var lat1 = loc1.lat;
+    var lon1 = loc1.lng;
+    var lat2 = loc2.lat;
+    var lon2 = loc2.lng;
+    
+    var R = 6371; // km
+    var dLat = (lat2-lat1).toRad();
+    var dLon = (lon2-lon1).toRad();
+    var lat1 = lat1.toRad();
+    var lat2 = lat2.toRad();
+
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c;
+    
+    return d / 1.609344;
 }
 
 $(document).ready(function () {
