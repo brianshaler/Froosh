875b6bb1e7ea131a99cb5ec206909b0cd362c328
Brian Shaler <brian@shaler.name>
2011-08-14 14:14:23 -0400

    mobile

--- public/js/froosh/froosh.js
+++ public/js/froosh/froosh.js
@@ -2,6 +2,11 @@
 var lat = null;
 var lng = null;
 
+function showView (view) {
+    $(".autohide").hide();
+    $("#"+view).show();
+}
+
 function requestLocation () {
     navigator.geolocation.getCurrentPosition(gotLocation, deniedLocation, { enableHighAccuracy: true });
 }
@@ -12,8 +17,43 @@ function gotLocation (pos) {
     
     $("#lat").html(pos.coords.latitude);
     $("#lng").html(pos.coords.longitude);
+    
+    showView("askForLocation");
+    getNearby(lat, lng);
+}
+
+function getNearby (lat, lng) {
+    $.ajax({
+        url: "http://froo.sh/restaurant/nearby.json",
+        dataType: "json",
+        data: {lat: lat, lng: lng},
+        success: gotNearby
+    });
+}
+
+function gotNearby (data) {
+    $("#results").html("<ul id=\"resultList\"></ul>");
+    data.forEach(function (restaurant) {
+        var li = $("<li>");
+        li.html(restaurant.name);
+        $("#resultList").appendChild(li);
+    });
+    var li = $("<li>");
+    li.html("last...");
+    $("#resultList").appendChild(li);
 }
 
 function deniedLocation () {
     // :-3(
+    showView("requiresLocation");
 }
+
+$(document).ready(function () {
+   $(".findStuff").click(function (e) {
+       e.preventDefault();
+       showView("loading");
+       requestLocation();
+       return false;
+   });
+    
+});
