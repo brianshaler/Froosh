dbda1348e38c77f1e4c096f7d29f721b5fafae3e
Brian Shaler <brian@shaler.name>
2011-08-20 02:24:33 -0700

    duhhhhh

--- conf/production.js
+++ conf/production.js
@@ -4,7 +4,7 @@
  */
 module.exports = function(app,express) {
 		
-	app.set('db-uri', 'mongodb://localhost/froosh');
+	app.set('db-uri', 'mongodb://localhost/froosh-dev');
     app.use(express.errorHandler({ dumpExceptions: true, showStack: false }));
 	
 }
