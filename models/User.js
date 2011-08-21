cc36eab3eb4f276e43d111f8dc04567cea107354
Brian Shaler <brian@shaler.name>
2011-08-21 16:57:35 -0700

    add authentication methods and new user fields

--- models/User.js
+++ models/User.js
@@ -10,6 +10,7 @@ var mongoose = require('mongoose'),
 
 var SUPERADMIN = "superadmin";
 var ADMIN = "admin";
+var MODERATOR = "moderator";
 var USER = "user";
 var BANNED = "banned";
 
@@ -18,18 +19,39 @@ var User = new Schema({
     // Single default property
     user_name:{type: String, required: true, index: true},
     password: {type:String},
+    display_name:{type: String},
+    email: {type: String},
     zip: {type: String},
     status: {type: String},
     last_activity: {type: Date, default: Date.now},
-    session_key: {type: String},
+    session_key: {type: String, index: true},
     session_start: {type: Date},
     created_at: {type: Date, default: Date.now}
     
 });
 
-User.methods.authenticate = function (password) {
+User.methods.registerUser = function () {
+    this.status = USER;
+    this.display_name = this.user_name;
+}
+
+User.methods.setPassword = function (p, autoSave) {
+    this.password = this.hash(p);
+    if (autoSave === true) {
+        this.save();
+    }
+}
+
+User.methods.getDisplayName = function () {
+    if (this.display_name && this.display_name.length > 0) {
+        return this.display_name;
+    } else {
+        return this.user_name;
+    }
+}
+
+User.methods.authenticate = function (p) {
     var u = this.user_name;
-    var p = password;
     var h = this.password;
     var auth = false;
     
@@ -52,17 +74,20 @@ User.methods.getSessionKey = function () {
 
 User.methods.generateSessionKey = function () {
     var key = "";
-    key = "/KEY-"+this.user_name+"-KEY/";
+    key = "/KEY-"+this.user_name+Date.now()+"-KEY/";
     return key;
 }
 
-User.methods.validate = function (key, token) {
+User.methods.validateSessionToken = function (key, token) {
     var validated = false;
+    console.error("Trying: key: "+key+"; token: "+token);
+    console.error("Against: key: "+this.session_key+"; token: "+this.generateToken());
     if (this.session_key && this.session_key != "" && key && token && key != "" && token != "") {
         if (key === this.session_key && this.generateToken() === token) {
             validated = true;
         }
     }
+    console.error("Validation: "+this.user_name+": "+(validated ? "true" : "false"));
     return validated;
 }
 
@@ -77,11 +102,21 @@ User.methods.isSuperAdmin = function () {
 }
 
 User.methods.isAdmin = function () {
-    return this.status === ADMIN || this.status === SUPERADMIN ? true : false;
+    return (this.status === ADMIN || 
+            this.status === SUPERADMIN) ? true : false;
+}
+
+User.methods.isModerator = function () {
+    return (this.status === MODERATOR || 
+            this.status === ADMIN || 
+            this.status === SUPERADMIN) ? true : false;
 }
 
 User.methods.isUser = function () {
-    return this.status === USER || this.status === ADMIN || this.status === SUPERADMIN ? true : false;
+    return (this.status === USER || 
+            this.status === MODERATOR || 
+            this.status === ADMIN || 
+            this.status === SUPERADMIN) ? true : false;
 }
 
 User.methods.hash = function (str) {
