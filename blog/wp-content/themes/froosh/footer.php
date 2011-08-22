4c4b31ee502507ace6a6c01985091895c91bb8eb
Brian Shaler <brian@shaler.name>
2011-08-21 19:45:38 -0700

    css & html changes for wp theme

--- blog/wp-content/themes/froosh/footer.php
+++ blog/wp-content/themes/froosh/footer.php
@@ -1,5 +1,5 @@
     	</div>		
-			<footer>
+			<footer class="page-footer">
 				&copy; 2011 <a href="/">froo.sh</a> | <a href="http://nyc.startupweekend.org/2011/08/14/august-2011-startup-weekend-teams/">nyc startup weekend</a> | <a href="#">about</a> | <a href="/?mobile=true">mobile site</a>
 			</footer>
     </div>
